e0d8849e7c95c0f860a75d9505a3fc8fe6a97998
Tyler Melan <tymelan@gmail.com>
2025-03-26 19:27:31 -0700

    handle errors with fputs and fclose

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <syslog.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+
+void log_and_print_error(const char *error_message)
+{
+  const char *format_string = "%s: %s";
+
+  fprintf(stderr, format_string, error_message, strerror(errno));
+  fprintf(stderr, "\n");
+  syslog(LOG_ERR, format_string, error_message, strerror(errno));
+}
 
 int main(int argc, char *argv[])
 {
   openlog(NULL, 0, LOG_USER);
 
-  if (argc != 3)
+  if (argc < 3)
   {
-    printf("Usage: %s <writefile> <writestr>\n", argv[0]);
-    printf("  <writefile> - file path.\n");
-    printf("  <writestr> - text string that will be written to the file.\n");
+    fprintf(stderr, "Usage: %s <writefile> <writestr>\n", argv[0]);
+    fprintf(stderr, "  <writefile> - file path.\n");
+    fprintf(stderr, "  <writestr> - text string that will be written to the file.\n");
     syslog(LOG_ERR, "Invalid number of arguments: %d", argc);
     return 1;
   }
@@ -17,19 +29,33 @@ int main(int argc, char *argv[])
   char *writefile = argv[1];
   char *writestr = argv[2];
 
-  FILE *fp = fopen(writefile, "w");
+  syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
 
+  FILE *fp = fopen(writefile, "w");
   if (fp == NULL)
   {
-    perror("The file could not be created or written to");
-    syslog(LOG_ERR, "Could not open file for writing: %s", writefile);
+    log_and_print_error("Error opening file for writing");
     return 1;
   }
 
-  fputs(writestr, fp);
-  syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
+  bool file_operation_error = false;
 
-  fclose(fp);
+  if (fputs(writestr, fp) == EOF)
+  {
+    log_and_print_error("Error writing to file");
+    file_operation_error = true;
+  }
+
+  if (fclose(fp) == EOF)
+  {
+    log_and_print_error("Error closing file");
+    file_operation_error = true;
+  }
+
+  if (file_operation_error)
+  {
+    return 1;
+  }
 
   return 0;
 }
