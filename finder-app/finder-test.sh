30d8c1a7eeb753e2377a1857633d39e5897d9486
Tyler Melan <tymelan@gmail.com>
2025-04-13 16:59:23 -0700

    update finder-test to use executables on path and conf in /etc/finder-app/conf

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,12 +5,11 @@
 set -e
 set -u
 
-cd `dirname $0`
-
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFDIR=/etc/finder-app/conf
+username=$(cat "$CONFDIR"/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -34,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat "$CONFDIR"/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -53,10 +52,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
 
